eadd8a9d979fee1de015aae942e0da1691ae9019
Hoa-N-1212 <hnguyen47@wsutech.edu>
2025-11-17 12:13:51 -0600

    Add files via upload

---
+++ HNguyen Test Driven Python/Module 1/HNguyen Refreshing Assignment/refreshingAssignment.py
@@ -0,0 +1,123 @@
+baseChoices = ["water", "sbrite", "pokeacola", "mr. salt", "hill fog", "leaf wine"]
+flavorChoices = ["lemon", "cherry", "strawberry", "mint", "blueberry", "lime"]
+# --- --- ---
+# forced to interpret several getter functions as the instructions for them are vague and don't go into detail
+# what they do or how much something would cost
+# each choice above should be paired with a price in an object and the rest of the assignment would have to be reworked
+# --- --- --- 
+
+# Order class
+class Order:
+    _items = []
+
+    def __init__(self,drinks):
+        if not isinstance(drinks,list):
+            print("items must be in a list")
+            return
+        self._items=drinks
+        
+
+#getters
+    def get_item(self,index):
+        return self._items[index]
+    
+    def get_all_items(self):
+        return self._items
+    
+    def get_num_items(self):
+        return self._items.count()
+    
+    # vague instructions assuming total means each base and flavor choice has a set price through the POS system
+    def get_total(self):   
+        print("not implemented as it requires what each base and flavor would cost")
+        #total = 0
+        #for item in self._items:
+        #    # iterate through each base and add the price for whichever base is given
+        #    if item.get_base == :
+        #       total += 
+        #   #iterate through all flavors in list and add to total
+        #   for item in self._items:
+        #      for flavor in item.get_flavors():
+        #         match flavor:
+        #                case water:
+        #                    total +=
+        #return total
+        
+
+    def get_reciept(self):
+        recieptString = ""
+        for item in self._items:
+            recieptString = recieptString + item.get_base() + " with " + ", ".join(item.get_flavors()) + "\n"
+        return recieptString
+
+#setters
+    def add_item(self, drink):
+        if not isinstance(drink,Drink):
+            print(f"{drink} is not a drink instance")
+        self._items.append(drink)
+    
+    def remove_item(self,index):
+        self._items.pop(index)
+
+# Drink class
+class Drink:
+    _base = str
+    _flavors = [str]
+    def __init__(self,base,flavors):
+        if not isinstance(flavors,list):
+            print("flavors must be in a list")
+            return
+        self._base = base
+        self._flavors = flavors
+
+    #getters
+    def get_base(self):
+        return self._base
+    
+    def get_flavors(self):
+        return self._flavors
+    
+    def get_num_flavors(self):
+        return self._flavors.count
+    
+    #setters
+    def set_base(self,newBase):
+        self.base = newBase
+
+    #checks for duplicate in the array, skips intruction if does
+    def set_flavors(self,flavorArray):
+        for item in flavorArray:
+            if item not in flavorChoices:
+                print(f"flavor not in flavor choices: {item}")
+                return
+        if duplicateCheck(flavorArray):
+            print(f"duplicate of {item} in given list")
+            return
+        self._flavors = flavorArray
+
+    def add_flavors(self,flavor):
+        if flavor in self._flavors:
+            print(f"flavor already in list: {flavor}")
+            return
+        self._flavors.append(flavor)
+
+# misc 
+def duplicateCheck(list):
+    already_seen = {}  
+    for item in list:
+        if item in already_seen:
+            return True
+        already_seen[item] = 0
+    return False
+
+# --- --- ---
+# no interactivity was required on the assignment, only if the getters and setters are correct
+# tests are not required for this assignment
+# assignment forced to be either in text or web URL, cannot be sent as file
+# --- --- ---
+
+drinker = Drink("water",["lemon"])
+drinker.set_flavors(["lemon","strawberry","blueberry"])
+drinker2 = Drink("sbrite",["lemon","lime"])
+order1 = Order([drinker,drinker2])
+print(order1.get_reciept())
